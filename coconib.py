8b12922189a233e9fb612908d429514b9ce4d81b
Toaster-Gaming <127230597+Toaster-Gaming@users.noreply.github.com>
2023-03-07 08:19:11 -0800

    Add files via upload

---
+++ coconib.py
@@ -0,0 +1,31 @@
+print("COCONIB OS, DOORS 7 PROFESSIONAL, LOADING......")
+newPass = True
+if newPass == True:
+    print("You need a new password!")
+    newPasswordSetting = input("What would you like your new password to be? Type it here: ")
+    Password = newPasswordSetting
+    newPass = False
+print("You need to verify to get in!")
+loginStatus = input("Enter Password: ")
+if loginStatus == Password:
+    print("Logging in.... Verifying account... Loading and Preparing COCONIB OS... Loading DOORS 7 PROFESSIONAL...")
+    print("Welcome to Doors 7 Professional, run by COCONIB OS!")
+else:
+    for i in range(99999999999999):
+        print("YOU GOT YOUR OWN PASSWORD WRONG  RIP BOZO 💀")
+def OS():
+    print("What would you like to do today? We have 'OHIO 💀 Simulator', 'Only in OHIO 💀', and 'ohio💀tube.org'.")
+    god = input("Open Application: ")
+    if god == "OHIO 💀 Simulator":
+        for i in range(99999999999999999):
+            print("Things like this only happen in OHIO 💀.")
+    elif god == "Only in OHIO 💀":
+        print("Please pay $5.99 to purchase the 'BEING ABLE TO PLAY PACK'. Thank you.")
+        for i in range(999999999999999999):
+            print("We hope you enjoyed your new DOORS 7 PROFESSIOAL. Unfortunatly, a trojan in the form of 'OHIO 💀' games and applications has hacked this device. Thank you for purchasing DOORS 7 PROFESSIONAL. NO REFUNDS")
+    elif god == "ohio💀tube.org":
+        print("Search up 'Frozen: The RemakeBoot' on youtube.com for the full experience of ohio💀tube.org.")
+    else:
+        print("I'm afraid that is not an option.")
+        OS()
+OS()
