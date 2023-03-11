cd6ff6d9297e1d9cd2b5287cf08e6e87015e433f
Toaster-Gaming <127230597+Toaster-Gaming@users.noreply.github.com>
2023-03-10 19:38:51 -0800

    Update coconib.py

--- coconib.py
+++ coconib.py
@@ -1,18 +1,4 @@
 print("COCONIB OS, DOORS 7 PROFESSIONAL, LOADING......")
-newPass = True
-if newPass == True:
-    print("You need a new password!")
-    newPasswordSetting = input("What would you like your new password to be? Type it here: ")
-    Password = newPasswordSetting
-    newPass = False
-print("You need to verify to get in!")
-loginStatus = input("Enter Password: ")
-if loginStatus == Password:
-    print("Logging in.... Verifying account... Loading and Preparing COCONIB OS... Loading DOORS 7 PROFESSIONAL...")
-    print("Welcome to Doors 7 Professional, run by COCONIB OS!")
-else:
-    for i in range(99999999999999):
-        print("YOU GOT YOUR OWN PASSWORD WRONG  RIP BOZO 💀")
 def OS():
     print("What would you like to do today? We have 'OHIO 💀 Simulator', 'Only in OHIO 💀', and 'ohio💀tube.org'.")
     god = input("Open Application: ")
@@ -28,4 +14,44 @@ def OS():
     else:
         print("I'm afraid that is not an option.")
         OS()
-OS()
+def login():
+    userlogin = input("Have you used this device before? (y/n)  ")
+    if userlogin == "n":
+      newPass = True
+      if newPass == True:
+        user = input("You need a username, or user! What would you like it to be? Enter Here: ")
+        usernames = ["Kevin"]
+        usernames.append(user)
+        print(f"The current users are: {usernames}")
+        print("You need a new password!")
+        newPasswordSetting = input("What would you like your new password to be? Type it here: ")
+        Password = newPasswordSetting
+        newPass = False
+        print("You need to verify to get in!")
+        loginStatus = input("Enter Password: ")
+        if loginStatus == Password:
+            print("Logging in.... Verifying account... Loading and Preparing COCONIB OS... Loading DOORS 7 PROFESSIONAL...")
+            print("Welcome to Doors 7 Professional, run by COCONIB OS!")
+            OS()
+        else:
+            for i in range(99999999999999):
+                print("YOU GOT YOUR OWN PASSWORD WRONG RIP BOZO 💀")
+                print("YOU GOT YOUR OWN PASSWORD WRONG RIP BOZO 💀")
+    if userlogin == "y":
+        loginUser = input("Username: ")
+        if loginUser in usernames:
+            print("Password: ")
+login()
+def emergencyshutdown():
+    time = input("We hope you had a fun experience with COCONIB OS DOORS 7 PROFESSIONAL. Unfortunatly, the 'OHIO', apps have hacked the software, and deleted most of your information. To keep your information safe, we will need to shut down this PC. Confirm (y/n): )")
+    if time == "y":
+        for i in range(99999900000000000000000000000000000):
+            print("SHUTTING DOWN...")
+    elif time == "n":
+        for i in range(9999999999999999999999999999999):
+          print("THIS IS THE OHIO GAMES. WE HAVE TAKEN OVER YOUR PC.")
+    else:
+        print("That is not an option")
+        emergencyshutdown()
+emergencyshutdown()
+
